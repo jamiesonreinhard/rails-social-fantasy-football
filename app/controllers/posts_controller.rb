0320f51af7e51cb447445f3f86052ba27c9d5656
Jamieson Reinhard <jamieson.reinhard2@gmail.com>
2020-10-05 19:06:15 -0600

    added posts model and controller. can now create new posts that are tied to a user and a league

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,27 @@
+class PostsController < ApplicationController
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @leagues = League.all
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :author, :content, :league_id, :user_id)
+  end
+
+end
