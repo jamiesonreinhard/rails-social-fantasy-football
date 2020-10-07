9ca6f9d6646640c6e43fa8cb4eb0ad7d8dc03472
Jamieson Reinhard <jamieson.reinhard2@gmail.com>
2020-10-07 13:50:08 -0600

    added avatar image to user and can now set avatar image from the edit user page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def user_avatar(user, size=40)
+    if user.avatar.attached?
+      user.avatar
+    else
+      current_user.gravatar_url
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :teams, dependent: :destroy
   has_many :posts
+  has_one_attached :avatar
+  include Gravtastic
+  gravtastic
 end
